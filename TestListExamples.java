6de2fe8a548aeb08e0edad40ca95d7178f1c09d5
scbova <sbova@ucsd.edu>
2023-11-06 17:10:33 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,49 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test(timeout = 500)
+  public void testMerge2() {
+    List<String> left = Arrays.asList("a", "d", "z");
+    List<String> right = Arrays.asList("b", "c");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "b", "c", "d", "z");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMerge3() {
+    List<String> left = Arrays.asList("a", "b");
+    List<String> right = Arrays.asList("c", "d");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test
+  public void testFilter() {
+    List<String> input = Arrays.asList("Hello", "Hi", "moon");
+    IsMoon s = new IsMoon();
+    List<String> filtered = ListExamples.filter(input, s);
+    List<String> expected = Arrays.asList("moon");
+    assertEquals(expected, filtered);
+  }
+
+  @Test
+  public void testFilter2() {
+    List<String> input = Arrays.asList("Hello", "Hi");
+    IsMoon s = new IsMoon();
+    List<String> filtered = ListExamples.filter(input, s);
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, filtered);
+  }
+
+  @Test
+  public void testFilter3() {
+    List<String> input = Arrays.asList("moon", "Hi", "moon");
+    IsMoon s = new IsMoon();
+    List<String> filtered = ListExamples.filter(input, s);
+    List<String> expected = Arrays.asList("moon", "moon");
+    assertEquals(expected, filtered);
+  }
 }
